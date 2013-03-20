959143bffe5ab70732ebd42a3a6adc88ef49a225
Ronaldo Barnes <krazydragon@fullsail.com>
2013-03-19 20:54:29 -0700

    Week three started

---
+++ gen/com/j2w3/rbarnes/seattlecoffeelocator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.j2w3.rbarnes.seattlecoffeelocator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
